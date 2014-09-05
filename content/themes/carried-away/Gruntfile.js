3783b58547033e47a13f30453c5196f954baa35f
ac-bristol <andrewcocker86@googlemail.com>
2014-09-05 10:04:20 +0100

    updated grunt config to include compass

--- content/themes/carried-away/Gruntfile.js
+++ content/themes/carried-away/Gruntfile.js
@@ -1,11 +1,21 @@
 module.exports = function(grunt) {
   var jsFileList = [
     //'js/libs/responsiveslides.min.js',
-    //'js/libs/respond.min.js'
+    '<%= meta.srcPath %>javascript/libs/jquery.1.11.1.js',
+    '<%= meta.srcPath %>javascript/libs/respond.min.js',
+    '<%= meta.srcPath %>javascript/navigation.js',
+    '<%= meta.srcPath %>javascript/skip-link-focus-fix.js'
+    //'<%= meta.srcPath %>javascript/customizer.js'
   ];
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    meta: {
+      srcPath: 'src/',
+      buildPath: 'assets/',
+      projectName: 'Carried Away',
+      projectNameSpace: 'carried-away',
+    },
     concat: {
       options: {
         stripBanners: false,
@@ -14,7 +24,7 @@ module.exports = function(grunt) {
       },
       dist: {
         src: [jsFileList],
-        dest: 'js/build.js',
+        dest: '<%= meta.buildPath %>javascript/build.js',
       },
     },
     uglify: {
@@ -22,60 +32,60 @@ module.exports = function(grunt) {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       build: {
-        src: 'js/main.js',
-        dest: 'js/main.min.js'
+        src: '<%= meta.srcPath %>javascript/main.js',
+        dest: '<%= meta.buildPath %>javascript/main.min.js'
       }
     },
-    sass: {                              // Task
-      dist: {                            // Target
-        options: {                       // Target options
-          style: 'expanded'
-        },
-        files: [{
-            expand: true,
-            cwd: 'sass',
-            src: ['main.scss'],
-            dest: 'css',
-            ext: '.css'
-        }]
-      }
+    compass: {
+      dev: {
+          options: {              
+            sassDir: '<%= meta.srcPath %>scss',
+            cssDir: '<%= meta.buildPath %>styles',
+            imagesDir: '<%= meta.buildPath %>images',
+            javascriptsDir: '<%= meta.buildPath %>scripts',    
+            fontsDir: '<%= meta.buildPath %>fonts',
+            outputStyle: 'expanded',
+            sourcemap: true
+          }
+      },
+      build: {
+        options: {              
+          sassDir: '<%= meta.srcPath %>scss',
+          cssDir: '<%= meta.buildPath %>styles',
+          imagesDir: '<%= meta.buildPath %>images',
+          javascriptsDir: '<%= meta.buildPath %>scripts',    
+          fontsDir: '<%= meta.buildPath %>fonts',
+          outputStyle: 'compressed',
+          environment: 'production'
+        }
+      },
     },
     watch: {
       uglify: {
-        files: ['js/main.js'],
+        files: ['<%= meta.srcPath %>javascript/main.js'],
         tasks: ['uglify'],
       },
       sass: {
       // We watch and compile sass files as normal but don't live reload here
-        files: ['sass/*.scss'],
-        tasks: ['sass'],
-      },
-      livereload: {
-        // Browser live reloading
-        // https://github.com/gruntjs/grunt-contrib-watch#live-reloading
-        options: {
-          livereload: false
-        },
-        files: [
-          'sass/*',
-          'css/main.css',
-          'js/main.js',
-          'templates/*.php',
-          '*.php'
-        ]
+        files: ['<%= meta.srcPath %>scss/*.scss'],
+        tasks: ['compass:dev'],
       }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-compass');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   //grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-sass');
   //grunt.loadNpmTasks('grunt-casperjs');
 
-  // Default task(s).
-  grunt.registerTask('default', ['concat','uglify', 'sass']);
+  // Default task.
+  grunt.registerTask('default', ['compass:dev','concat']);
+
+  // Build Task
+  grunt.registerTask('build', ['compass:build','uglify', 'concat']);
 
 };
