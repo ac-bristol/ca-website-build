4be151acfe55aac025a36fd9a5401fd418e01744
ac-bristol <andrewcocker86@googlemail.com>
2014-09-11 11:06:51 +0100

    initial loop set uup for products

--- content/themes/carried-away/front-page.php
+++ content/themes/carried-away/front-page.php
@@ -15,7 +15,7 @@ get_header(); ?>
 <div class="container clearfix">
 
 	<div class="full-width">
-		<div class="full-image">
+		<div class="inner image-only">
 			<img src="<?php echo get_bloginfo('template_directory');?>/assets/images/main-image.jpg" alt="Carried away" />
 		</div>
 	</div>
@@ -25,13 +25,13 @@ get_header(); ?>
 <div class="container clearfix">
 
 	<div class="half-width">
-		<div class="full-image">
+		<div class="inner image-only">
 			<img src="<?php echo get_bloginfo('template_directory');?>/assets/images/bags.jpg" alt="stack of bags" />
 		</div>
 	</div>
 	
 	<div class="half-width">
-		<div class="full-text">
+		<div class="inner">
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php the_content(); ?>
@@ -44,6 +44,38 @@ get_header(); ?>
 
 <div class="container clearfix">
 
+	<div class="full-width">
+		<div class="inner">
+		Products
+<?php
+
+$paged = (get_query_var('page')) ? get_query_var('page') : 1;
+
+$args = array(
+	'post_type' => 'product',
+	'order' => 'asc',
+	'posts_per_page' => 1,
+	'paged' => $paged
+);
+$query = new WP_Query( $args );
+
+while ( $query->have_posts() ) {
+	$query->next_post();
+	echo '<li>' . get_the_title( $query	->post->ID ) . '</li>';
+} ?>
+
+<div id="pagination">
+	<ul class="pagination">
+		<li id="next-stories" class="arrow">
+			<?php next_posts_link( 'Load more >>', $query->max_num_pages ); ?>
+		</li>
+	</ul>
+</div><!-- /#pagination -->
+
+<?php wp_reset_postdata(); ?>
+		</div>
+	</div>
+	
 </div>
 
 
